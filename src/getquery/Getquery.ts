03d9aec1bc1a9b2408db9b985a7a7bafa7617d4f
haseeb1965 <haseebsiddiqui1965@gmail.com>
2023-04-05 22:31:26 +0500

    updated getquery

--- src/getquery/Getquery.ts
+++ src/getquery/Getquery.ts
@@ -12,21 +12,40 @@ export const GetQuery = async ({ model, text }: data) => {
     "Content-Type": "application/json",
     Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
   };
-  const datag = {
-    model: model,
-    messages: [{ role: "user", content: text }],
-    temperature: 0.7,
-  };
-  const url = "https://api.openai.com/v1/chat/completions";
-  const options = {
-    method: "POST",
-    headers: headers,
-    body: JSON.stringify(datag),
-  };
-  const res = await fetch(url, options);
-  const datas = await res.json();
+  if (model.includes("gpt")) {
+    const data1 = {
+      model: model,
+      messages: [{ role: "user", content: text }],
+      temperature: 0.7,
+    };
+    const url = "https://api.openai.com/v1/chat/completions";
+    const options = {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data1),
+    };
+    const res = await fetch(url, options);
+    const datas = await res.json();
 
-  const message = await datas.choices[0].message.content;
+    const message = await datas.choices[0].message.content;
 
-  return message;
+    return message;
+  } else {
+    const datag = {
+      model: model,
+      prompt: text,
+      temperature: 0.7,
+    };
+    const url = "https://api.openai.com/v1/completions";
+    const options = {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(datag),
+    };
+    const res = await fetch(url, options);
+    const datas = await res.json();
+
+    const message = await datas.choices[0].text;
+    return message;
+  }
 };
